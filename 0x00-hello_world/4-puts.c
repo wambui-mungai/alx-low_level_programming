b2bb3d5bb64fe8700cb363c6605850acefad2505
wambui-mungai <wambuim220@gmail.com>
2023-02-16 12:30:05 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <studio.h>
+
+
 /**
  * main - Entry point
  *
- * Return:Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle'):
-		return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
