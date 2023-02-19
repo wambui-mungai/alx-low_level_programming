19c30324f355ead4a8abca36228be98b7fed15e6
wambui-mungai <wambuim220@gmail.com>
2023-02-19 01:56:05 -0800

    tebaphpla

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,14 +3,14 @@
  *main-print if the number is postive, zero, or negative
  *Description: using the main function
  *this program prints "Programming is positive, zero, or negative
- *Return:0
+ *retun(0)
  */
 int main(void)
 {
 int num;
-for (num = '0'; num  <= '9'; 0++)
+for (num = 0; num  <= 9; 0++)
 {
-putchar((num % 10) + '0')
+putchar(num)
 }
 putchar('\n');
 return (0);
