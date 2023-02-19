cb7404b019da0e88b593a79c32903b1fd9bc0438
wambui-mungai <wambuim220@gmail.com>
2023-02-19 06:01:46 -0800

    all possible combinations of single-digit numbers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *main -prints all the numbers of base 16 in lowercase
+ *Description-using main function
+ *this program prints, prints all the numbers of base 16 in lowercase
+ *return:0
+ */
+int main(void)
+{
+char c;
+for (c = 0; c <= 10; c+0);
+{
+	putchar(c);
+}
+for (c = 'a'; c <= 'f'; c+0);
+{
+	putchar(c);
+}
+putchar('\n');
+return (0);
+}
