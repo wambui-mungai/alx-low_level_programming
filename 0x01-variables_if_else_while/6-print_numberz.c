25ef2c8d918c848624e6ed4af42fa461a2fd2942
wambui-mungai <wambuim220@gmail.com>
2023-02-19 00:49:10 -0800

    print numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
 /**
- *main - Prints all single digit numbers of base 10 starting from 0,
- *- only using putchar and without char variables.
- *Return: 0
+ * main - print if the number is postive, zero, or negative
+ * Description: using the main function
+ * this program prints "Programming is positive, zero, or negative
+ * Return: 0
  */
 int main(void)
 {
-char c;
-for (c = '0'; c <= '9'; c++)
-{
-	putchar(c);
-}
+for (num = 0; num < 10; num++)
+putchar((num % 10) + '0');
 putchar('\n');
 return (0);
 }
