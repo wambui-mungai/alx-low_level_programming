ecf236af723f11409a5debf676c5853ebfef3406
wambui-mungai <wambuim220@gmail.com>
2023-02-22 10:44:45 -0800

    multiples

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Lists all the natural numbers below 1024 (excluded)
+ *        that are multiples of 3 or 5.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+int c, sum = 0;
+
+for (c = 0; c < 1024; c++)
+{
+if ((c % 3) == 0 || (c % 5) == 0)
+sum += c;
+}
+
+printf("%d\n", sum);
+
+return (0);
+}
