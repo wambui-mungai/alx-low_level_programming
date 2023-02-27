0c77b3ceeb5df04d112352cd9f769c9d951e5e0f
wambui-mungai <wambuim220@gmail.com>
2023-02-27 13:43:03 -0800

    6-puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+ * put2 - function declaration
+ * @str: string pointer
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int index;
+
+	for (index = 0; index < _strlen(str); index++)
+	{
+		if (index % 2 == 0)
+		{
+			_putchar(str[index]);
+		}
+	}
+	_putchar('\n');
+}
+
