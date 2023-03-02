36775d9f4c442288cc1b0be335679f636d66d428
wambui-mungai <wambuim220@gmail.com>
2023-03-01 18:17:30 -0800

    0-strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strcat - concatenates two strings,
+ * @dest: destination.
+ * @src: source.
+ * Return: the pointer to dest.
+ */
+char *_strcat(char *dest, char *src)
+
+{
+	int count = 0;
+	int count1 = 0;
+
+	while (*dest + count != '\0')
+	{
+		count++;
+	}
+	while (count1 >= 0)
+	{
+		*(dest + count) = *(src + count1);
+		if (*(src + count1) == '\0')
+		count++;
+		count1++;
+	}
+	return (dest);
+}
+
