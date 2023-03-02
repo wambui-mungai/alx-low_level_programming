5ce72d9c582f0adff4adbbd5642c651bbd3a4d32
wambui-mungai <wambuim220@gmail.com>
2023-03-01 18:26:49 -0800

    0-strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,22 +6,21 @@
  * Return: the pointer to dest.
  */
 char *_strcat(char *dest, char *src)
-
 {
-	int count = 0;
-	int count1 = 0;
+	int count = 0, count1 = 0;
 
-	while (*dest + count != '\0')
+	while (*(dest + count) != '\0')
 	{
 		count++;
 	}
+
 	while (count1 >= 0)
 	{
 		*(dest + count) = *(src + count1);
 		if (*(src + count1) == '\0')
+			break;
 		count++;
 		count1++;
 	}
 	return (dest);
 }
-
