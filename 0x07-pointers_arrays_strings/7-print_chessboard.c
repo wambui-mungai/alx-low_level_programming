b0acf18e3c39ca015d40ff028190465bb1996074
wambui-mungai <wambuim220@gmail.com>
2023-03-06 13:30:53 -0800

    chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_chessboard - function definition
+ * @a: the pointer array
+ * Returns: the void
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int r;
+	int c;
+
+	for (r = 0; r < 8; r++)
+	{
+		for (c = 0; c < 8; c++)
+		{
+			_putchar(a[r][c]);
+		}
+		_putchar('\n');
+	}
+}
+
