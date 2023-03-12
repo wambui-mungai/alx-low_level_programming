db73618f8e1c5f8c1a49ddd255f0b0d2888e9cbc
wambui-mungai <wambuim220@gmail.com>
2023-03-12 09:57:34 -0700

    all arguments

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -3,7 +3,8 @@
 
 int main(int argc, char *argv[]);
 int _putchar(char c);
-int main(int argc, char *argv[]__attribute__((unused)))
+int main(int argc, char *argv[]__attribute__((unused)));
+int main(int argc, char *argv[]);
 
 #endif
 
