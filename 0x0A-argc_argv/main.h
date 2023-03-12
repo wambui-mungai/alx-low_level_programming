2e8a1f9571896ee999fdf47cc0750a35e863e157
wambui-mungai <wambuim220@gmail.com>
2023-03-12 10:04:56 -0700

    multiplication

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -5,6 +5,7 @@ int main(int argc, char *argv[]);
 int _putchar(char c);
 int main(int argc, char *argv[]__attribute__((unused)));
 int main(int argc, char *argv[]);
+int main(int argc, char *argv[]);
 
 #endif
 
