5c943823d4d4ff2e253df614b4b5837199d0b006
wambui-mungai <wambuim220@gmail.com>
2023-04-03 20:03:48 -0700

    list length

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -10,7 +10,7 @@ size_t listint_len(const listint_t *h)
 	unsigned int i = 0;
 	const listint_t *new_node = h;
 
-	while (new_node->next != NULL)
+	for (; new_node; new_node = new_node->next)
 		i++;
 	return (i);
 }
